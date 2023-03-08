c442750792a9195a11e890f2dafdfc9f20d4d9bc
Marco Boccia <pokermark95@live.it>
2023-03-08 17:53:54 +0100

    finiti numeri <= 5 diventano 0 e somma di tutti i numeri

---
+++ matrici.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <random>
+using namespace std;
+
+int main()
+{
+    int righe = 9;
+    int colonne = 9;
+    int somma = 0;
+    int matrice[righe][colonne];
+
+    for (int i = 0; i < righe; i++)
+    {
+        for (int j = 0; j < colonne; j++)
+        {
+            matrice[i][j] = rand() % 10;
+
+            /* i numeri <=5 diventano 0
+                if (matrice[i][j] <= 5)
+                {
+                    matrice[i][j] = 0;
+                }
+            */
+           // mi sommo tutti i numeri della matrice
+                somma += matrice[i][j];
+           
+        }
+    }
+
+    cout << "    ";
+    for (int i = 0; i < colonne; i++)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+    int trattini = colonne * 2 + 4;
+    for (int i = 0; i < trattini; i++)
+    {
+        cout << "-";
+    }
+    cout << endl;
+
+    for (int i = 0; i < righe; i++)
+    {
+
+        cout << i << "|  ";
+
+        for (int j = 0; j < colonne; j++)
+        {
+            cout << matrice[i][j] << " ";
+        }
+
+        cout << endl;
+    }
+    cout << "La somma dei numeri nella matrice è: "<< somma << endl;
+}
